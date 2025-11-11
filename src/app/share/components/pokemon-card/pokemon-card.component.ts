b3486217c5efb3dca8dadf6d694b7ac3dea3e7cd
Bruno <brmartins92@gmail.com>
2025-11-11 10:18:37 -0300

    completed

--- src/app/share/components/pokemon-card/pokemon-card.component.ts
+++ src/app/share/components/pokemon-card/pokemon-card.component.ts
@@ -19,7 +19,7 @@ export interface PokemonCardData {
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass'],
-  host: { '(click)': 'onClick()' }
+  host: { '(click)': 'onClick()' },
 })
 export class PokemonCardComponent {
   @Input() data!: PokemonCardData;
@@ -27,35 +27,52 @@ export class PokemonCardComponent {
 
   constructor(private pokemonService: PokemonService) {}
 
-
-
   onClick() {
-
     if (this.data) {
-      console.log("card");
+      console.log('card');
 
-      this.pokemonService.getPokemonEvolutionChain(this.data.number).subscribe(evolutions => {
-        const evoList = evolutions.map((evo, idx) => ({
-          name: evo.name,
-          image: evo.image,
-          order: idx + 1
-        }));
+      this.pokemonService
+        .getPokemonEvolutionChain(this.data.number)
+        .subscribe((evolutions) => {
+          const evoList = evolutions.map((evo, idx) => ({
+            name: evo.name,
+            image: evo.image,
+            order: idx + 1,
+          }));
 
-        this.pokemonService.getPokemon(this.data.number).subscribe(pokemonDetail => {
-          // Suporte para array de objetos ou strings
-          const abilities: string[] = Array.isArray(pokemonDetail.abilities)
-            ? (pokemonDetail.abilities as any[]).map(ab => typeof ab === 'string' ? ab : ab.ability?.name || ab.name || '')
-            : [];
-          const types: string[] = Array.isArray(pokemonDetail.types)
-            ? (pokemonDetail.types as any[]).map(t => typeof t === 'string' ? t : t.type?.name || t.name || '')
-            : [];
-          // type (singular) para compatibilidade com classe dinâmica
-          const type = types.length > 0 ? types[0] : '';
-          const cardWithEvo: PokemonCardData = { ...this.data, evolutions: evoList, abilities, types, type };
+          this.pokemonService
+            .getPokemon(this.data.number)
+            .subscribe((pokemonDetail) => {
+              // Suporte para array de objetos ou strings
+              const abilities: string[] = Array.isArray(pokemonDetail.abilities)
+                ? (pokemonDetail.abilities as any[]).map((ab) =>
+                    typeof ab === 'string'
+                      ? ab
+                      : ab.ability?.name || ab.name || ''
+                  )
+                : [];
+              const height = pokemonDetail.height ? pokemonDetail.height  : 0;
+              const weight = pokemonDetail.weight ? pokemonDetail.weight : 0;
+              const types: string[] = Array.isArray(pokemonDetail.types)
+                ? (pokemonDetail.types as any[]).map((t) =>
+                    typeof t === 'string' ? t : t.type?.name || t.name || ''
+                  )
+                : [];
+              // type (singular) para compatibilidade com classe dinâmica
+              const type = types.length > 0 ? types[0] : '';
+              const cardWithEvo: PokemonCardData = {
+                ...this.data,
+                evolutions: evoList,
+                abilities,
+                types,
+                type,
+                height,
+                weight,
+              };
 
-          this.select.emit(cardWithEvo);
+              this.select.emit(cardWithEvo);
+            });
         });
-      });
     }
   }
 }
